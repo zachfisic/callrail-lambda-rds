9379df633d1e22e2e86497002bf9a8ceff62dddf
Zach Fisic <zachfisic@gmail.com>
2018-10-25 03:44:16 -0700

    Modify request structure to allow for limits on parallel requests

--- cron/callrail.js
+++ cron/callrail.js
@@ -2,18 +2,13 @@ const fetch = require('node-fetch');
 const qs = require('query-string');
 const util = require('./util.js');
 
-const BASE_URL = 'https://api.callrail.com';
+const BASE_URI = 'https://api.callrail.com';
 const CALLRAIL_ACCESS_TOKEN = process.env.CALLRAIL_ACCESS_TOKEN
 const CALLRAIL_ACCOUNT_ID_1 = process.env.CALLRAIL_ACCOUNT_ID_1
 
 
-async function getCalls(account, urlParams) {
 
-	let path = `/v2/a/${account}/calls.json`;
-	let url = BASE_URL + path;
-	if (urlParams) {
-		url += '?' + qs.stringify(urlParams);
-	}
+async function getCalls(uri) {
 	let options = {
 		method: 'GET',
 		headers: {
@@ -22,7 +17,7 @@ async function getCalls(account, urlParams) {
 		}
 	};
 	try {
-		let response = await fetch(url, options);
+		let response = await fetch(uri, options);
 		let data = await response.json();
 		return data;
 	}
@@ -31,15 +26,74 @@ async function getCalls(account, urlParams) {
 	}
 }
 
+
+function buildUri(account, uriParams) {
+	let path = `/v2/a/${account}/calls.json`;
+	let uri = BASE_URI + path;
+	if (uriParams) {
+		uri += '?' + qs.stringify(uriParams);
+	}
+	return uri;
+}
+
+
+function bindUri(account, uriParams) {
+	return function(page) {
+		if (uriParams && page) {
+			uriParams.page = page;
+		}
+		return buildUri(account, uriParams);
+	}
+}
+
+
+async function parallelLimit(uris, limit = 10) {
+  let inFlight = new Set();
+  
+  return uris.map(async (uri, i) => {
+    // Hold the loop by another loop
+    // while the next promise resolves
+    while(inFlight.size >= limit) {
+      await Promise.race(inFlight);
+    }
+    
+    console.log(`STARTING ROUND->${i} SIZE->${inFlight.size}`);
+
+    const promise = getCalls(uri);
+
+    // Add promise to inFlight Set
+    inFlight.add(promise);
+    // Delete promise from Set when it is done
+    await promise;
+    inFlight.delete(promise);
+
+    console.log(`ENDING ROUND->${i} SIZE->${inFlight.size}`);
+  });
+};
+
+
+
 module.exports.calls = async (event, context) => {
 
 	let { today, yesterday } = util.getTimeframe();
 
-	let urlParams = {
+	let dateParams = {
 		"start_date": today,
 		"end_date": yesterday
 	};
 
-	let result = await getCalls(CALLRAIL_ACCOUNT_ID_1, urlParams);
-	return result.total_pages;
+	buildRbUri = bindUri(CALLRAIL_ACCOUNT_ID_1, dateParams);
+	rbUri = buildRbUri();
+
+	let firstPageResults = await getCalls(rbUri);
+	
+	remainingUris = [];
+	for (let i = 2; i <= firstPageResults.total_pages; i++) {
+		remainingUris.push(buildRbUri(i))
+	}
+
+	let promises = await parallelLimit(remainingUris, 3);
+  let results = await Promise.all(promises);
+
+  return results.length;
 }
